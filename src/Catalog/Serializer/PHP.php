a167276b5aad0644826433e3cdb2e9700830b1e8
Javier Marín <javier@marinros.com>
2023-10-14 16:42:34 +0200

    feat: improve PHP catalog serializer

--- src/Catalog/Serializer/PHP.php
+++ src/Catalog/Serializer/PHP.php
@@ -8,20 +8,43 @@ use ideatic\l10n\Project;
 
 class PHP extends ArraySerializer
 {
-    /** @inheritDoc */
-    public function generate(array $domains, \stdClass|Project $config = null): string
-    {
-        $phpArray = var_export($this->_generate($domains), true);
+  /** @inheritDoc */
+  public function generate(array $domains, \stdClass|Project $config = null): string
+  {
+    // $phpArray = var_export($this->_generate($domains), true);
+    $phpArray = ['['];
+    foreach ($domains as $domain) {
+      foreach ($domain->strings as $strings) {
+        $string = reset($strings);
 
-        $php = ['<?php'];
-        if (!$this->comments) {
-            $this->comments = 'Created ' . date('r');
+        $stringID = $string->fullyQualifiedID();
+        if ($this->locale) {
+          $translation = $domain->translator->getTranslation($string, $this->locale, false);
+
+          if (isset($translation)) {
+            if ($string->comments) {
+              $phpArray[] = '  ' . var_export($stringID, true) . ' /* ' . $string->comments . ' */ => ' . var_export($translation, true) . ',';
+            } else {
+              $phpArray[] = '  ' . var_export($stringID, true) . ' => ' . var_export($translation, true) . ',';
+            }
+          }
+        } else {
+          $phpArray[] = '  ' . var_export($stringID, true) . ' => ' . var_export(null, true) . ',';
         }
-        $php[] = "/* {$this->comments} */";
-        $php[] = '';
-        $php[] = "return {$phpArray};";
-
-        return implode(PHP_EOL, $php);
+      }
     }
+    $phpArray [] = ']';
+    $phpArray = implode(PHP_EOL, $phpArray);
+
+    $php = ['<?php'];
+    if (!$this->comments) {
+      $this->comments = 'Created ' . date('r');
+    }
+    $php[] = "/* {$this->comments} */";
+    $php[] = '';
+    $php[] = "return {$phpArray};";
+
+    return implode(PHP_EOL, $php);
+  }
 }
 
--- src/Utils/ICU/Pattern.php
+++ src/Utils/ICU/Pattern.php
@@ -11,70 +11,76 @@ use const U_ZERO_ERROR;
 
 class Pattern
 {
-    /** @var array<string|Placeholder>  */
-    public array $nodes = [];
+  /** @var array<string|Placeholder> */
+  public array $nodes = [];
 
-    public function __construct(string $pattern)
-    {
-        (new Parser())->parse($pattern, $this);
+  public function __construct(string $pattern)
+  {
+    (new Parser())->parse($pattern, $this);
+  }
+
+  public function hasPlaceholders(): bool
+  {
+    foreach ($this->nodes as $node) {
+      if ($node instanceof Placeholder) {
+        return true;
+      }
     }
 
-    public function hasPlaceholders(): bool
-    {
-        foreach ($this->nodes as $node) {
-            if ($node instanceof Placeholder) {
-                return true;
+    return false;
+  }
+
+  /**
+   * @return array<string>
+   */
+  public function textNodes(): array
+  {
+    $textNodes = [];
+    if ($this->nodes) {
+      foreach ($this->nodes as &$node) {
+        if ($node instanceof Placeholder) {
+          if (is_array($node->content)) {
+            foreach ($node->content as $subIcuMessage) {
+              foreach ($subIcuMessage->textNodes() as &$textNode) {
+                $textNodes[] = &$textNode;
+              }
             }
+          }
+        } elseif (is_string($node)) {
+          $textNodes[] = &$node;
         }
+      }
+    }
+    return $textNodes;
+  }
 
-        return false;
+  /**
+   * @param array<string, mixed>  $args
+   */
+  public function format(string $locale, array $args = []): string
+  {
+    if (!class_exists(MessageFormatter::class, false)) {
+      throw new InvalidArgumentException('MessageFormatter class not found, please check PHP Intl extension is available');
     }
 
-    public function textNodes(): array
-    {
-        $textNodes = [];
-        if ($this->nodes) {
-            foreach ($this->nodes as &$node) {
-                if ($node instanceof Placeholder) {
-                    if (is_array($node->content)) {
-                        foreach ($node->content as $subIcuMessage) {
-                            foreach ($subIcuMessage->textNodes() as &$textNode) {
-                                $textNodes[] = &$textNode;
-                            }
-                        }
-                    }
-                } elseif (is_string($node)) {
-                    $textNodes[] = &$node;
-                }
-            }
-        }
-        return $textNodes;
+    $fmt = new MessageFormatter($locale, $this->render());
+    if ($fmt->getErrorCode() != U_ZERO_ERROR) {
+      throw new InvalidArgumentException('Invalid ICU message: ' . $fmt->getErrorMessage());
+    }
+    return $fmt->format($args);
+  }
+
+  public function render(bool $prettyPrint = true): string
+  {
+    $strings = [];
+    foreach ($this->nodes as $node) {
+      if (is_string($node)) {
+        $strings[] = $node;
+      } else {
+        $strings[] = $node->render($prettyPrint);
+      }
     }
 
-    public function format(string $locale, array $args = []): string
-    {
-        if (!class_exists(MessageFormatter::class, false)) {
-            throw new InvalidArgumentException('MessageFormatter class not found, please check PHP Intl extension is available');
-        }
-
-        $fmt = new MessageFormatter($locale, $this->render());
-        if ($fmt->getErrorCode() != U_ZERO_ERROR) {
-            throw new InvalidArgumentException('Invalid ICU message: ' . $fmt->getErrorMessage());
-        }
-        return $fmt->format($args);
-    }
-
-    public function render(bool $prettyPrint = true): string
-    {
-        $strings = [];
-        foreach ($this->nodes as $node) {
-            if (is_string($node)) {
-                $strings[] = $node;
-            } else {
-                $strings[] = $node->render($prettyPrint);
-            }
-        }
-
-        return implode('', $strings);
-    }
+    return implode('', $strings);
+  }
 }
