7a53b766ab4ab87893d951951ffe86ac5e22d278
Javier Marín <javier@marinros.com>
2023-11-08 21:28:52 +0100

    test: improved html parser tests

--- tests/HtmlParserTest.php
+++ tests/HtmlParserTest.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
@@ -7,9 +8,9 @@ use PHPUnit\Framework\TestCase;
 class HtmlParserTest extends TestCase
 {
 
-    public function testBasic()
-    {
-        $input = '<html lang="en">
+  public function testBasic()
+  {
+    $input = '<html lang="en">
 <head>
   <meta charset="utf-8">
   <title>Title</title>
@@ -20,18 +21,35 @@ class HtmlParserTest extends TestCase
 </body>
 </html>';
 
-        $dom = HTML_Parser::parse($input);
+    $dom = HTML_Parser::parse($input);
 
-        $this->assertTrue($dom->findAll('body')[0] instanceof HTML_Parser_Element);
-        $this->assertEquals($input, $dom->render());
-    }
+    $this->assertTrue($dom->findAll('body')[0] instanceof HTML_Parser_Element);
+    $this->assertEquals($input, $dom->render());
+  }
 
-    public function testBasic2()
-    {
-        $input = '<!doctype html><html lang="es"><head><meta charset="utf-8"><title>Chuletator</title><base href="/"><meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width"><link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700,800,300italic,400italic,700italic,800italic" rel="stylesheet"><link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"><link rel="manifest" href="manifest.webmanifest"><meta name="theme-color" content="#1976d2"><link rel="icon" type="image/png" sizes="192x192" href="/assets/app/assets/images/icon-192x192.png"><link rel="icon" type="image/png" sizes="192x192" href="/assets/app/assets/images/icon-96x96.png"><link rel="icon" type="image/x-icon" href="favicon.ico"><link rel="stylesheet" href="/assets/app/es/styles.194d4444cfa18a8d36d6.css"></head><body></body></html>';
+  public function testBasic2()
+  {
+    $input = '<!doctype html><html lang="es"><head><meta charset="utf-8"><title>Chuletator</title><base href="/"><meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width"><link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700,800,300italic,400italic,700italic,800italic" rel="stylesheet"><link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"><link rel="manifest" href="manifest.webmanifest"><meta name="theme-color" content="#1976d2"><link rel="icon" type="image/png" sizes="192x192" href="/assets/app/assets/images/icon-192x192.png"><link rel="icon" type="image/png" sizes="192x192" href="/assets/app/assets/images/icon-96x96.png"><link rel="icon" type="image/x-icon" href="favicon.ico"><link rel="stylesheet" href="/assets/app/es/styles.194d4444cfa18a8d36d6.css"></head><body></body></html>';
 
-        $dom = HTML_Parser::parse($input);
+    $dom = HTML_Parser::parse($input);
 
-        $this->assertEquals($input, $dom->render());
-    }
+    $this->assertEquals($input, $dom->render());
+  }
+
+  public function testAngular()
+  {
+    $input = '<div #mainContainer class="tab-content" [style.min-height.px]="sameHeight ? tabHeight : undefined">
+          <ng-content/>
+      </div>
+      <>
+      < invalidTag>
+      @if (!(autoHideDots && tabs.length <= 1)) {
+          <ol role="tablist">
+              <li *ngFor="let tab of tabs" role="tab" (click)="selectTab(tab)" [class.active]="tab.active" [attr.aria-selected]="tab.active" [attr.aria-label]="tab.header"></li>
+          </ol>
+      }';
+
+    $dom = HTML_Parser::parse($input, true);
+    $this->assertEquals($input, $dom->render());
+  }
 }
